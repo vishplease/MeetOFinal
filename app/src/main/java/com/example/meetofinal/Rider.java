12dc980e9a2b70a090720a517ade1649d522c3d6
vishplease <vishalch@umich.edu>
2019-11-12 16:19:26 -0500

    Some Activities Added

---
+++ app/src/main/java/com/example/meetofinal/Rider.java
@@ -0,0 +1,15 @@
+package com.example.meetofinal;
+
+public class Rider {
+
+    public String uniqname, password;
+
+    public Rider(){
+
+    }
+
+    public Rider(String uniqname, String password) {
+        this.uniqname = uniqname;
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/example/meetofinal/TripRequest.java
@@ -0,0 +1,25 @@
+package com.example.meetofinal;
+
+import java.sql.Timestamp;
+
+public class TripRequest {
+    public String Rider, startLocation, endLocation;
+    public Timestamp requestedTime;
+    public int handBag, carryOn, checkIn;
+    public boolean status;
+
+    public TripRequest() {
+
+    }
+
+    public TripRequest(String rider, String startLocation, String endLocation, Timestamp requestedTime, int handBag, int carryOn, int checkIn, boolean status) {
+        Rider = rider;
+        this.startLocation = startLocation;
+        this.endLocation = endLocation;
+        this.requestedTime = requestedTime;
+        this.handBag = handBag;
+        this.carryOn = carryOn;
+        this.checkIn = checkIn;
+        this.status = status;
+    }
+}
