ea72d1b9bf268576b301a8ffb70f882184eba4fd
vishplease <vishalch@umich.edu>
2019-11-12 09:54:35 -0500

    Some Activities Added

---
+++ app/src/main/java/com/example/meetofinal/TravelReturnTrip.java
@@ -0,0 +1,14 @@
+package com.example.meetofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TravelReturnTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_return_trip);
+    }
+}
