49340caa09f1b7e6c79d2b9a38af5adecdbfef47
vishplease <vishalch@umich.edu>
2019-11-12 15:52:09 -0500

    Some Activities Added

--- app/src/main/java/com/example/meetofinal/MatchInProgress.java
+++ app/src/main/java/com/example/meetofinal/MatchInProgress.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UpcomingTrips extends AppCompatActivity {
+public class MatchInProgress extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upcoming_trips);
+        setContentView(R.layout.activity_match_in_progress);
     }
 }
--- app/src/main/java/com/example/meetofinal/MatchedTripSummary.java
+++ app/src/main/java/com/example/meetofinal/MatchedTripSummary.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UpcomingTrips extends AppCompatActivity {
+public class MatchedTripSummary extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upcoming_trips);
+        setContentView(R.layout.activity_matched_trip_summary);
     }
 }
