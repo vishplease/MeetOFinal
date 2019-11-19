6f0866b3ddad680b6fc01fef10d3cfc11515119b
Rahul Bhardwaj <rahul@Rahuls-MacBook-Pro.local>
2019-11-19 17:41:08 -0500

    Added screen titles for clarity; new trip button on Upcoming Trips screen

--- app/src/main/java/com/example/meetofinal626/UpcomingTrips.java
+++ app/src/main/java/com/example/meetofinal626/UpcomingTrips.java
@@ -2,13 +2,28 @@ package com.example.meetofinal626;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class UpcomingTrips extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class UpcomingTrips extends AppCompatActivity implements View.OnClickListener {
+
+    FloatingActionButton buttonAddTrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upcoming_trips);
+
+        buttonAddTrip = findViewById(R.id.buttonAddTrip);
+        buttonAddTrip.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(UpcomingTrips.this, TravelOneWay.class);
+        startActivity(intent);
     }
 }
