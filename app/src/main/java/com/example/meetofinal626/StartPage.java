0d7c782dd39098b4745db76f7b2049e2b2a8436b
Rahul Bhardwaj <rahul@Rahuls-MacBook-Pro.local>
2019-11-19 16:41:36 -0500

    Made Start Page the Launcher Activity, set navigation between Start and Trip Creation Activity

--- app/src/main/java/com/example/meetofinal626/StartPage.java
+++ app/src/main/java/com/example/meetofinal626/StartPage.java
@@ -2,13 +2,27 @@ package com.example.meetofinal626;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StartPage extends AppCompatActivity {
+public class StartPage extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonGetStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+
+        buttonGetStarted = findViewById(R.id.buttonGetStarted);
+        buttonGetStarted.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(StartPage.this, TravelOneWay.class);
+        startActivity(intent);
     }
 }
