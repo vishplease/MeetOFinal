d072517a6975e7f72861d08997302805f2d372eb
vishplease <vishalch@umich.edu>
2019-11-19 12:24:45 -0500

    Some Activities Added

--- app/src/main/java/com/example/meetofinal/MainActivity.java
+++ app/src/main/java/com/example/meetofinal/MainActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 //testing
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 }
