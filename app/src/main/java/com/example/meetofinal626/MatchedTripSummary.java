6271fb0a25340631a5f6d9ce518bdb96220b9cdc
rahul123bhardwaj <rahul123bhardwaj@gmail.com>
2019-12-09 11:00:23 -0500

    Created a dummy GroupTrip object for Hikaru to proceed

--- app/src/main/java/com/example/meetofinal626/MatchedTripSummary.java
+++ app/src/main/java/com/example/meetofinal626/MatchedTripSummary.java
@@ -6,16 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.sql.Timestamp;
+
 public class MatchedTripSummary extends AppCompatActivity {
 
     TextView textViewRidingWith, textViewRidingMembers;
-
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matched_trip_summary);
 
+        GroupTrip gt = new GroupTrip(new String[]{"-LvfEKD9-wGpV5-6IvVM", "-Lvclh-w5XdOnuLnIKWh"}, timestamp);
         Intent intent =getIntent();
         String message = intent.getStringExtra("Extra_Message");
         TextView textView = findViewById(R.id.textViewRidingMembers);
