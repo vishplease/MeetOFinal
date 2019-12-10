b2605ffdfd919ecea2307bfa881da32c0891f01e
hikaruozeki <ozeki@umich.edu>
2019-12-09 21:20:00 -0500

    Trip Summary Design Change ver2

--- app/src/main/java/com/example/meetofinal626/MatchedTripSummary.java
+++ app/src/main/java/com/example/meetofinal626/MatchedTripSummary.java
@@ -1,16 +1,33 @@
 package com.example.meetofinal626;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class MatchedTripSummary extends AppCompatActivity {
 
-    TextView textViewRidingWith, textViewRidingMembers;
+    TextView textViewRidingWith, textViewRidingMembers,textViewRidingMembers2;
+    TextView textViewTripSummary, textViewLeave, textViewTimeSchedule, textViewLuggage;
+    TextView textViewNumber1, textViewNumber2, textViewNumber3;
+    ImageView imageView0, imageView2, imageView4;
     Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
     @Override
@@ -18,10 +35,50 @@ public class MatchedTripSummary extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matched_trip_summary);
 
-        GroupTrip gt = new GroupTrip(new String[]{"-LvfEKD9-wGpV5-6IvVM", "-Lvclh-w5XdOnuLnIKWh"}, timestamp);
-        Intent intent =getIntent();
-        String message = intent.getStringExtra("Extra_Message");
-        TextView textView = findViewById(R.id.textViewRidingMembers);
-        textView.setText(message);
+        textViewRidingMembers = findViewById(R.id.textViewRidingMembers);
+        textViewRidingWith = findViewById(R.id.textViewRidingWIth);
+        textViewTripSummary = findViewById(R.id.textViewTripSummary);
+        textViewLeave = findViewById(R.id.textViewLeave);
+        textViewTimeSchedule = findViewById(R.id.textViewTimeSchedule);
+        textViewRidingMembers2 = findViewById(R.id.textViewRidingMembers2);
+        textViewLuggage = findViewById(R.id.textViewLuggage);
+        textViewNumber1 = findViewById(R.id.textViewNumber1);
+        textViewNumber2 = findViewById(R.id.textViewNumber2);
+        textViewNumber3 = findViewById(R.id.textViewNumber3);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference().child("triprequests");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Log.e("MatchedTripSummary",snapshot.getKey());
+                    if (snapshot.getKey().equals("-LvhRaahW5g51esX-7ZF")) {
+                        GroupTrip foundGroupTrip = snapshot.getValue(GroupTrip.class);
+                        String findGroupTrip = foundGroupTrip.riderID;
+                        textViewRidingMembers.setText(findGroupTrip);
+
+                    } else if (snapshot.getKey().equals("-LvhRaam-ObUAFJ1tqMC")) {
+                        GroupTrip foundGroupTrip = snapshot.getValue(GroupTrip.class);
+                        String findGroupTrip2 = foundGroupTrip.riderID;
+                        textViewRidingMembers2.setText(findGroupTrip2);
+
+                    }
+                    Calendar calendar = Calendar.getInstance();
+                    String currentDate = DateFormat.getDateInstance().format(calendar.getTime());
+
+                    TextView textViewTimeSchedule = findViewById(R.id.textViewTimeSchedule);
+                    textViewTimeSchedule.setText(currentDate);
+
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
