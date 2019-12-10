0650e625e936ec350b2c53380406caa87fbff03d
vishplease <vishalch@umich.edu>
2019-12-10 13:50:45 -0500

    formatted card view, recycler view works with database

--- app/src/main/java/com/example/meetofinal626/TripsAdapter.java
+++ app/src/main/java/com/example/meetofinal626/TripsAdapter.java
@@ -12,6 +12,8 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class TripsAdapter extends RecyclerView.Adapter<TripsAdapter.TripViewHolder>{
@@ -25,10 +27,11 @@ public class TripsAdapter extends RecyclerView.Adapter<TripsAdapter.TripViewHold
 		View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.trip_card, parent, false);
 
 		//when i click on it, where do i go - instead of traveloneway it should go to trip summary. Onclick get the group trip ID
+		//send to different intents conditionally based on status of the trip
 		v.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(v.getContext(), TravelOneWay.class);
+				Intent intent = new Intent(v.getContext(), MatchedTripSummary.class);
 				v.getContext().startActivity(intent);
 			}
 		});
@@ -39,8 +42,38 @@ public class TripsAdapter extends RecyclerView.Adapter<TripsAdapter.TripViewHold
 	//rendering of the card, populated with the trip details happens here
 	@Override
 	public void onBindViewHolder(@NonNull TripViewHolder holder, int position) {
+		//format time to show up properly in cardview
+		SimpleDateFormat timeformatter = new SimpleDateFormat("hh:mm a");
+		Time cardTime = new Time(trips.get(position).requestedTime);
+		String cardTimeOutput = timeformatter.format(cardTime);
+
+		//format date to show up properly in cardview
+
+		SimpleDateFormat dateformatter = new SimpleDateFormat("MM/dd/yy");
+		Date cardDate = new Date(trips.get(position).requestedTime);
+		String cardDateOutput = dateformatter.format(cardDate);
+
+		//rename pending to MATCH IN PROGRESS
+		String tripStatus = String.valueOf(trips.get(position).status);
+
+		if (tripStatus.equals("Pending")){
+			tripStatus = "MATCH IN PROGRESS";
+		}
+
+
+
+
+
+
 		//gets position of the trip in the recycler view, finds an item on the card (refer to trip card xml)
-		holder.tripStatus.setText(String.valueOf(new Time(trips.get(position).requestedTime)));
+		holder.textViewTripTime.setText(cardTimeOutput);
+		holder.textViewStatus.setText(tripStatus);
+		holder.textViewTripDate.setText(cardDateOutput);
+		holder.textViewTripStart.setText(String.valueOf(trips.get(position).startLocation).toUpperCase());
+		holder.textViewTripEnd.setText(String.valueOf(trips.get(position).endLocation).toUpperCase());
+
+
+
 	}
 
 	@Override
@@ -55,12 +88,20 @@ public class TripsAdapter extends RecyclerView.Adapter<TripsAdapter.TripViewHold
 	//
 	public static class TripViewHolder extends RecyclerView.ViewHolder {
 		CardView cv;
-		TextView tripStatus;
+		TextView textViewStatus,
+				textViewTripDate,
+				textViewTripStart,
+				textViewTripEnd,
+				textViewTripTime;
 
 		TripViewHolder(View itemView) {
 			super(itemView);
 			cv = itemView.findViewById(R.id.cv);
-			tripStatus = (TextView)itemView.findViewById(R.id.textViewStatus);
+			textViewStatus = itemView.findViewById(R.id.textViewStatus);
+			textViewTripTime = itemView.findViewById(R.id.textViewTripTime);
+			textViewTripDate = itemView.findViewById(R.id.textViewTripDate);
+			textViewTripStart = itemView.findViewById(R.id.textViewTripStart);
+			textViewTripEnd = itemView.findViewById(R.id.textViewTripEnd);
 		}
 	}
 
